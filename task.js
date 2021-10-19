86d2db34125593cf707328a0bc81c78f74bb7b08
ammarshtayeh <76966536+ammarshtayeh@users.noreply.github.com>
2021-10-19 22:10:54 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,7 @@
+function geneerat() {
+    var image = document.createElement('img')
+    var div = document.getElementById('img-ggen');
+    image.src = "ansuu.png";
+    div.appendChild(image);
+
+}
